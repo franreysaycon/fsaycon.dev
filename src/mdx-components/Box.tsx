d785e32a235d6fc572247a200f05d22bae2f5cdc
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-11-02 15:59:20 +0800

    Create sample POC for mdx

---
+++ src/mdx-components/Box.tsx
@@ -0,0 +1,5 @@
+const Box = () => (
+    <div style={{padding: 20, backgroundColor: 'tomato'}} />
+)
+  
+export default Box
---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import matter from "gray-matter"
+import { serialize } from "next-mdx-remote/serialize"
+import { MDXRemote } from "next-mdx-remote"
+import { Box } from "../mdx-components"
+import path from "path"
+import fs from "fs"
+
+const Home = ({ source, data }) => (
+  <MDXRemote {...source} components={{ Box }} />
+)
+
+export const getStaticProps = async () => {
+  const fileContents = fs.readFileSync(path.join(process.cwd(), "src/mdx/index.mdx"), 'utf8')
+  const { data, content } = matter(fileContents)
+  
+  const source = await serialize(content, { scope: data })
+
+  return {
+    props: {
+      data,
+      source,
+    }
+  }
+}
+
+export default Home
