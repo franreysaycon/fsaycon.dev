91ba362fef0304f25b587f890adf2cdd5b5cd884
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-11-07 22:37:32 +0800

    Add sitemaps functionality

---
+++ next-sitemap-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteUrl: "https://fsaycon.dev",
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "*",
+      },
+    ],
+  },
+}
